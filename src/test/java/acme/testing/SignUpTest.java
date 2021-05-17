1afdcbca30bb5afdea5fdde092ac27392e8c4d2d
manguecor <manguecor@alum.us.es>
2021-05-17 12:55:43 +0200

    #9 Test successful

--- src/test/java/acme/testing/SignUpTest.java
+++ src/test/java/acme/testing/SignUpTest.java
@@ -1,20 +1,24 @@
-/*
- * SignUpTest.java
- *
- * Copyright (C) 2012-2021 Rafael Corchuelo.
- *
- * In keeping with the traditional purpose of furthering education and research, it is
- * the policy of the copyright owner to permit non-commercial use and redistribution of
- * this software. It has been tested carefully, but it is not guaranteed for any particular
- * purposes. The copyright owner does not offer any warranties or representations, nor do
- * they accept any liabilities with respect to them.
- */
-
 package acme.testing;
 
-public class SignUpTest {
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-	// This is a placeholder where you can introduce your own sign-up test
-  	// Note that it depends on your project-specific test class.
+public class SignUpTest extends AcmePlannerTest {
+
+	// Lifecycle management ---------------------------------------------------
+
+	// Test cases -------------------------------------------------------------
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/sign-up/positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveSignUp(final String username, final String password, final String name, final String surname, final String email) {
+		super.signUp(username, password, name, surname, email);
+		super.signIn(username, password);
+		super.signOut();
+	}
+
+	// Ancillary methods ------------------------------------------------------
 	
 }
---
+++ src/test/java/acme/testing/manager/task/ManagerTaskDeleteTest.java
@@ -0,0 +1,26 @@
+package acme.testing.manager.task;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.AcmePlannerTest;
+
+public class ManagerTaskDeleteTest extends AcmePlannerTest {
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/manager/task/delete.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void delete(final int recordIndex) {
+		super.signIn("manager", "manager");
+		
+		super.clickOnMenu("Manager", "List tasks");
+		
+		super.clickOnListingRecord(recordIndex);
+		
+		super.clickOnSubmitButton("Delete task");
+		
+		super.signOut();
+	}
+
+}
